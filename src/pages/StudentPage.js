d004c1a7407c980cfccea2164772e0a906ed2203
lpsaldana <lpsaldana1@gmail.com>
2024-01-02 19:49:16 -0500

    Add student and teacher pages

--- src/pages/StudentPage.js
+++ src/pages/StudentPage.js
@@ -1,7 +1,36 @@
-const StudentPage = () => {
+import { useEffect, useState } from "react";
+import axios from "axios";
+import StudentTable from "../components/StudentTable";
+
+const StudentPage = ({ user }) => {
+
+  const [subjects, setSubjects] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        `http://localhost:3001/student/${user.Id}`
+      );
+      setSubjects(response.data);
+    };
+
+    fetchData();
+  }, []);
+
+  const getAvrg = (grades) => {
+    let sum = 0;
+    grades.forEach((grade) => {
+      console.log(grade.grades[0].value);
+      sum += Number(grade.grades[0].value);
+    });
+    return(sum / grades.length);
+  };
+
   return (
     <div>
-      <h1>Student Page</h1>
+      <h1>Estudiante</h1>
+      <p>Hola {user.name}!</p>
+      <StudentTable subjects={subjects} getAverage={getAvrg} />
     </div>
   );
 };
--- src/pages/TeacherPage.js
+++ src/pages/TeacherPage.js
@@ -1,8 +1,79 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import TeacherTable from "../components/TeacherTable";
+
 const TeacherPage = ({ user }) => {
+  const [students, setStudents] = useState([]); // students is an array of strings
+  const [classrooms, setClassrooms] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [subjects, setSubjects] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        `http://localhost:3001/classrooms/teacher/${user.Id}`
+      );
+      setClassrooms(response.data.map((data) => data.name));
+      setSubjects(
+        response.data.map((data) => data.subjects.map((data) => data))
+      );
+      setStudents(
+        response.data.map((data) => data.students.map((data) => data))
+      );
+    };
+
+    fetchData();
+    setLoading(false);
+  }, []);
+
+  const updateGrade = async (studentId, subjectId, gradeValue) => {
+    try {
+      const response = await axios.put(
+        `http://localhost:3001/grade/${subjectId}/${studentId}`,
+        {
+          value: gradeValue,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleAddGrade = async (studentId, subjectId, gradeValue) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:3001/grade/${subjectId}/${studentId}`,
+        {
+          value: gradeValue,
+          teacher_id: user.Id,
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <h1>{user.name}</h1>
-      <h2>{user.email}</h2>
+      <h1>Teacher Page </h1>
+      <p>Hi {user.name}!</p>
+
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {classrooms.map((classroom, index) => (
+            <TeacherTable
+              classroom={classroom}
+              subjects={subjects[index]}
+              students={students[index]}
+              updateGrade={updateGrade}
+              addGrade={handleAddGrade}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
